0e78bb9b3b6fd975fae88c088c56cea68c144dc5
luffytaro19 <tonnytang13@gmail.com>
2024-05-28 10:16:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def countdown():
   now = datetime.now()
   new_year = datetime(2025, 1, 1)
   time_to_new_year = new_year - now
-  return "{} days".format(time_to_new_year.days, time_to_new_year.seconds//3600,                  (time_to_new_year.seconds//60)%60)
+  return "{}".format(time_to_new_year.days, time_to_new_year.seconds//3600,                  (time_to_new_year.seconds//60)%60)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
